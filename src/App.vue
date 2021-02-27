6d21cfaa0bec2b01561ba96c637ee6727b14f6f9
riyadvd <riyadwvd3@gmail.com>
2021-02-27 13:02:34 +0530

    data

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,23 @@
   <div id="app">
     <Header />
     <videotab />
-    
   </div>
 </template>
 
 <style>
-  @import './style.css';
-  @import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css');
+@import "./style.css";
+@import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css");
 </style>
 
 <script>
-
-import Header from './components/Header'
-import videotab from './components/videotab'
+import Header from "./components/Header";
+import videotab from "./components/videotab";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Header,
     videotab,
-  }
-}
+  },
+};
 </script>
-
-
